269290a914d89d59b811099c4fe42d83f737b24d
reoneo97 <reoneo97@gmail.com>
2022-02-07 17:14:59 +0800

    completed simulation for idealpfr

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from loguru import logger
 
 
 
-temp = 373
+temp = 443
 pa_feed = 900
 M = 5
-vol = 500000  # Units of m3
+vol = 50000  # Units of m3
 rxn = ptsa_reaction
 
 time_interval = 0.1  # Time Intervals to perform simulation
-time_end = 300
+time_end = 100
 
-reactor_model = IdealCSTR(pa_feed, M, vol, temp, rxn)
+reactor_model = IdealPFR(pa_feed, M, vol, temp, rxn)
 reactor_model.run(time_interval, time_end)
--- simulator/reactor/pfr.py
+++ simulator/reactor/pfr.py
@@ -14,6 +14,7 @@ class IdealPFR(Reactor):
                  rxn_model: ReactionModel):
         # Initial Setup in base model
         super().__init__(pa_feed, M, vol, temperature, rxn_model)
+        self.init_logs()
 
     def run(self, time_interval: float = 0.1, time_end: float = 300):
         # Setup an array to log concentration at every interval
@@ -40,14 +41,14 @@ class IdealPFR(Reactor):
         conc_profile[:,2] = 0
         conc_profile[:,3] = 0
 
-        mol_profile = np.concatenate(
+        conc_profile = np.concatenate(
             [vol_intervals.reshape(-1, 1), conc_profile], axis=1)
+        self.log(conc_profile)
 
 
-        for t in ts[1:]:
-            new_conc_profile = np.zeros(shape=(vol_n,6))
+        for t in tqdm(ts[1:]):
+            new_conc_profile = np.zeros(shape=(vol_n,5))
             new_conc_profile[:,0] = vol_intervals
-            # vol_added = time_interval* self.flow_rate
             
             # Concentrations from previous interval 
             pa_conc, ipa_conc, ipp_conc, water_conc = conc_profile[0,1:]
@@ -70,7 +71,8 @@ class IdealPFR(Reactor):
             new_conc_profile[0,3] = ipp_amt/vol_step
             new_conc_profile[0,4] = water_amt/vol_step
 
-            for v in range(1,82):
+            for v in range(1,vol_n):
+                #Concentration from prev time interval
                 pa_conc, ipa_conc, ipp_conc, water_conc = conc_profile[v, 1:]
                 rate = self.reaction.get_rate(
                     ca=pa_conc, cb=ipa_conc, ce=ipp_conc, cw=water_conc,
@@ -79,19 +81,33 @@ class IdealPFR(Reactor):
 
                 dn = rate*time_interval*vol_step
                 
+                #Inflow from Upstream
                 pa_in = new_conc_profile[v-1,1]*time_interval*self.flow_rate
                 ipa_in = new_conc_profile[v-1,2]*time_interval*self.flow_rate
                 ipp_in = new_conc_profile[v-1,3]*time_interval*self.flow_rate
-                water_in = new_conc_profile[v-1, 4] * \
-                    time_interval*self.flow_rate
-
+                water_in = new_conc_profile[v-1, 4]*time_interval*self.flow_rate
+                #Mass Balance
                 pa_amt = pa_in + pa_conc*vol_step -pa_conc*time_interval*self.flow_rate - dn 
                 ipa_amt = ipa_in + ipa_conc*vol_step- ipa_conc*time_interval*self.flow_rate - dn
-                ipp_amt = ipp_conc*vol_step- ipp_conc*time_interval*self.flow_rate + dn
-                water_amt = water_conc*vol_step- water_conc*time_interval*self.flow_rate + dn
+                ipp_amt = ipp_in + ipp_conc*vol_step- ipp_conc*time_interval*self.flow_rate + dn
+                water_amt = water_in + water_conc*vol_step- water_conc*time_interval*self.flow_rate + dn
 
-    def init_logs(self,ts):
+                #Saving Concentration Values
+                new_conc_profile[v, 1] = pa_amt/vol_step
+                new_conc_profile[v, 2] = ipa_amt/vol_step
+                new_conc_profile[v, 3] = ipp_amt/vol_step
+                new_conc_profile[v, 4] = water_amt/vol_step
+            self.log(new_conc_profile)
+            conc_profile = new_conc_profile
+        pd.DataFrame(self.logs[1]).to_csv("initial.csv")
+
+        pd.DataFrame(new_conc_profile).to_csv("final.csv")
+
+        data = np.array(self.logs)
+        print(data.shape)
+
+    def init_logs(self):
         self.logs = []
     
     def log(self,conc_profile:np.array):
-        self.logs.append(conc_profile)
+        self.logs.append(conc_profile.copy())
