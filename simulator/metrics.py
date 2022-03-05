3de6c71b1f1554b1a1e914ba5075a6a774c1867e
Reo Neo <reo@Reos-MacBook-Air.local>
2022-03-05 11:33:54 +0800

    added metrics info

--- simulator/metrics.py
+++ simulator/metrics.py
@@ -3,8 +3,25 @@ from . import const as const
 # Metrics
 # Calculation of different optimization metrics
 
+# Fixed cost that will not change based on operating conditions
+FIXED_COST = 0
+
 
 def operating_cost(reactor: RealPFR):
+    """Function to calculate the operating cost that is required 
+
+    Args:
+        reactor (RealPFR): PFR Reactor object
+    """
+    # Pre-Reactor Costing
+    # feed_P =
+    # Condense Feed Stream into Liquid
+
+    # Pumping of IPA
+
+    # Pumping of Feed
+
+    # Heating of IPA and Feed Mixture
 
     ptsa_loading = reactor.flow_rate/1000*5  # m^3/ min - 5g dm3/min
     ptsa_cost = ptsa_loading*const.ptsa_cost_per_g
@@ -17,10 +34,26 @@ def operating_cost(reactor: RealPFR):
     ipp_out = reactor.pa_feed*conversion
     ipp_profit = ipp_out*const.ipp_cost
 
+    # Separation Calculations
+
+    # Cool Feed Down to Separation Conditions
+
+    # Heating Requirement
+
     pass
 
 
-def capital_cost():
+def find_P(T: float):
+    """Find pressure of feed required for the temperature
+
+    Args:
+        T (float): Desired temperature of the feed
+    """
+    pass
+
+
+def capital_cost(reactor: RealPFR, n_split: int):
+
     pass
 
 
